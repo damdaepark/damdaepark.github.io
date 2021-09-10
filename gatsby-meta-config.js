45bd20938ca2b04dea1a6cc9cf52aec824ba56b5
damdaepark <damdaepark@gmail.com>
2021-09-10 17:25:25 +0900

    icon changed

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,11 +2,11 @@ module.exports = {
   title: "Park's Blog", // Your website title
   description: "Park's Blog", // Your website description
   author: 'damdaepark', // Maybe your name
-  siteUrl: 'https://github.com/damdaepark/damdaepark.github.io', // Your website URL
+  siteUrl: 'damdaepark.dev', // Your website URL
   lang: 'en', // Language
   utterances: 'sungik-choi/gatsby-starter-apple-comment', // Github repository to store comments
   links: {
-    github: 'https://github.com/sungik-choi/gatsby-starter-apple', // Your github repository
+    github: 'https://github.com/damdaepark/damdaepark.github.io', // Your github repository
   },
   icon: 'src/images/icon.png', //  Favicon Path
 };
