83eca5ab739d259eb0a74cb162f8c6595eaefb56
damdaepark <damdaepark@gmail.com>
2021-10-04 15:32:16 +0900

    updated

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "Play Ground for PSE", // Your website title
+  title: "Playground for PSE", // Your website title
   description: "Park's Blog", // Your website description
   author: 'damdaepark', // Maybe your name
   siteUrl: 'https://github.com/damdaepark/damdaepark.github.io', // Your website URL
   lang: 'en', // Language
-  utterances: 'sungik-choi/gatsby-starter-apple-comment', // Github repository to store comments
+  utterances: 'damdaepark/github.io', // Github repository to store comments
   links: {
     github: 'https://github.com/damdaepark/', // Your github repository
   },
